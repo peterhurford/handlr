8144dd3a8ac6bc3e74e99378d23b2ebc0b17e261
abelcastilloavant <abel.castillo@avant.com>
2016-06-16 10:03:31 -0500

    redocument, style guilde

--- R/retry.R
+++ R/retry.R
@@ -1,24 +1,24 @@
 #' Try evaluating an expression multiple times before erroring.
 #'
 #' @param num_tries numeric. The number of attempts.
-#' @param sleep_secs numeric. How long to wait between attempts.
+#' @param sleep numeric. How long to wait between attempts.
 #' @param expr expression. The expression to evaluate with retries.
 #' @return TRUE if the expression gets evaluated successfully on some attempt.
 #' @export
-with_retries <- function(expr, num_tries = 1, sleep_secs = 0.001) {
+with_retries <- function(expr, num_tries = 1, sleep= 0.001) {
   num_tries <- max(num_tries, 1L)
   current_try <- 0
   while (current_try < num_tries) {
     out <- evaluate::try_capture_stack(expr, env = parent.frame())
-    if (!is(out, "error")) break
+    if (!methods::is(out, "error")) break
     current_try <- current_try + 1
-    message(paste0('Attempt ', current_try, ' failed.'))
+    message(paste0("Attempt ", current_try, " failed."))
     if (current_try < num_tries) {
-      cat(' Sleeping for ', sleep_secs, 'seconds\n')
+      cat("Sleeping for ", sleep, "seconds\n")
       Sys.sleep(sleep_secs)
     }
   }
-  if (is(out, "error")) {
+  if (methods::is(out, "error")) {
     stop(out)
   } else {
     out
