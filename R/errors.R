5aa07ea18bfa3f4c508c2c609ca4fd028e0a4fb4
Peter Hurford <peter@peterhurford.com>
2016-06-16 11:10:20 -0500

    scope functions for lintr

--- R/errors.R
+++ R/errors.R
@@ -13,19 +13,19 @@ with_errors_as <- errors_are <- function(are, exp) {
 #'
 #' @param exp expresion. The expression to run.
 #' @export
-errors_are_na <- function(exp) { errors_are(NA, exp) }
+errors_are_na <- function(exp) { handlr::errors_are(NA, exp) }
 
 #' Convert errors to NULL.
 #'
 #' @param exp expresion. The expression to run.
 #' @export
-errors_are_null <- function(exp) { errors_are(NULL, exp) }
+errors_are_null <- function(exp) { handlr::errors_are(NULL, exp) }
 
 #' Convert errors to FALSE.
 #'
 #' @param exp expresion. The expression to run.
 #' @export
-errors_are_false <- function(exp) { errors_are(FALSE, exp) }
+errors_are_false <- function(exp) { handlr::errors_are(FALSE, exp) }
 
 #' Convert errors to warnings.
 #'
